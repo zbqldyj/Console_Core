516a4df7476db6885acf674bfb225c1285900f7e
Administrator <Administrator@iZarw5zkma6jjpZ>
2019-12-11 10:22:36 +0800

    添加项目文件。

---
+++ Console_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
