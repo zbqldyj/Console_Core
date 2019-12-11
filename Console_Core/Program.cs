08f32c84f252958183a7269197c1088da53c4dce
Administrator <Administrator@iZarw5zkma6jjpZ>
2019-12-11 10:28:37 +0800

    修改错误

--- Console_Core/Program.cs
+++ Console_Core/Program.cs
@@ -6,7 +6,7 @@ namespace Console_Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
